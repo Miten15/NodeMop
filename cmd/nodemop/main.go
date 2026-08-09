ce486f43d2694e4d1b380eb513f1b95327072d6c
Zino <89517581+Miten15@users.noreply.github.com>
2026-08-09 22:13:27 +0530

    feat: add nodemop docker command

--- cmd/nodemop/main.go
+++ cmd/nodemop/main.go
@@ -7,10 +7,20 @@ import (
 
 	tea "charm.land/bubbletea/v2"
 
+	"github.com/Miten15/NodeMop/internal/dockertui"
 	"github.com/Miten15/NodeMop/internal/tui"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "docker" {
+		p := tea.NewProgram(dockertui.New())
+		if _, err := p.Run(); err != nil {
+			fmt.Fprintln(os.Stderr, "NodeMop Docker Mop failed:", err)
+			os.Exit(1)
+		}
+		return
+	}
+
 	root := "."
 	if len(os.Args) > 1 {
 		root = os.Args[1]
